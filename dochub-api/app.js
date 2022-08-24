6a5b7b5ee5151a9d81454628512f431c8d109381
Johannes Weber <johannes@drweber.de>
2022-08-23 18:07:12 -0700

    add session router

--- dochub-api/app.js
+++ dochub-api/app.js
@@ -9,6 +9,7 @@ require('./db/connection');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var sessionRouter = require('./routes/session');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/session', sessionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- dochub-api/routes/session.js
+++ dochub-api/routes/session.js
@@ -1,29 +1,23 @@
 const router = require("express").Router();
 const errorIfUnauthorized = require("../middleware/errorIfUnauthorized");
 
-module.exports = (db) => {
+const User = require('../db/User');
 
-  const users = require("../modules/users")(db);
+router.post("/login", (req, res, next) => {
+  const email = req.body.email;
+  User.findOne({ where: { email }})
+    .then(res.json)
+    .catch(next);
+});
 
-  router.post("/login", (req, res, next) => {
-    const email = req.body.email;
-    users.findByEmail(email)
-      .then(user => {
-        req.session.user = user;
-        res.json(user);
-      })
-      .catch(next);
-  });
+router.get("/logout", errorIfUnauthorized, (req, res) => {
+  const firstName = req.session.user.first_name;
+  res.end(`Goodbye, ${firstName}!`);
+  req.session = null;
+});
 
-  router.get("/logout", errorIfUnauthorized, (req, res) => {
-    const firstName = req.session.user.first_name;
-    res.end(`Goodbye, ${firstName}!`);
-    req.session = null;
-  });
+router.get("/me", errorIfUnauthorized, (req, res) => {
+  res.json(req.session.user);
+});
 
-  router.get("/me", errorIfUnauthorized, (req, res) => {
-    res.json(req.session.user);
-  });
-
-  return router;
-};
+module.exports = router;
