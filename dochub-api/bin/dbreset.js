c3493cee4b17a383bc65e9d9e9ce611661da78f3
Johannes Weber <johannes@drweber.de>
2022-08-26 18:14:15 -0700

    create default users

--- dochub-api/bin/dbreset.js
+++ dochub-api/bin/dbreset.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 require('../db/connection');
 
-
+const users = require('../modules/users');
 const User = require('../db/User');
-User.sync({force: true});
+User.sync({force: true})
+  .then(() => {
+    return users.create({name: "Johannes", email: "johannes@drweber.de", plaintextPassword: "12345" });
+  })
+  .then(() => {
+    return users.create({name: "Jess", email: "jess@drweber.de", plaintextPassword: "12345" });
+  });
+const Document = require('../db/Document');
+Document.sync({force: true});
