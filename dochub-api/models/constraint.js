5f907acd803944e53eda8e3b830f0e65b05e20b7
Johannes Weber <johannes@drweber.de>
2022-08-30 14:45:17 -0700

    add models constraint, constraintType

---
+++ dochub-api/models/constraint.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Constraint extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Constraint.belongsTo(models.ConstraintType, {
+        foreignKey: 'constraintTypeId',
+      })
+    }
+  }
+  Constraint.init({
+    label: DataTypes.STRING,
+    constraintTypeId: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Constraint',
+  });
+  return Constraint;
+};
