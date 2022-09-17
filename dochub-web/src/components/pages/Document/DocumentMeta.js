2ed88369e3e3ecfb869dfe82625b63aec7953906
Johannes Weber <johannes@drweber.de>
2022-09-17 12:14:31 -0700

    format Document view

--- dochub-web/src/components/pages/Document/DocumentMeta.js
+++ dochub-web/src/components/pages/Document/DocumentMeta.js
@@ -1,11 +1,24 @@
 import './Document.scss';
 
 export default function DocumentMeta ({document}) {
+
+  const authorMarkup = document.author && (
+    <div className="author"><span>Author: </span>{document.author}</div>
+  );
+
+  const descriptionMarkup = document.description && (
+    <div className="description">{document.description}</div>
+  )
+
   return (
     <>
       <div className="metaContainer">
         <h2>{document.title}</h2>
-        <span className="text-muted">Last modified: {new Date(document.updatedAt).toLocaleDateString()}</span>
+        <div className="detailsContainer">
+          {authorMarkup}
+          {descriptionMarkup}
+          <div className="text-muted">Last modified: {new Date(document.updatedAt).toLocaleDateString()} - {document.files.length} files</div>
+        </div>
       </div>
     </> 
   );
