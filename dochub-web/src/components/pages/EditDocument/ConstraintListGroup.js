b6503951f2013e064846337a6d9af5765868fef0
Johannes Weber <johannes@drweber.de>
2022-09-29 22:14:15 -0700

    address with MUI warnings

--- dochub-web/src/components/pages/EditDocument/ConstraintListGroup.js
+++ dochub-web/src/components/pages/EditDocument/ConstraintListGroup.js
@@ -7,24 +7,29 @@ export default function ConstraintListGroup(props) {
   const {constraints, toggleConstraint, document, constraintTypeId} = props; 
 
 
-  const [value, setValue] = useState([]);
+  const [value, setValue] = useState("");
 
   //const options = constraints.filter(item => item.constraintTypeId === constraintTypeId);
   return (
     <>
       <Autocomplete
-        placeholder='type or select'
         className='constraints-autocomplete'
+        clearOnEscape
         options={constraints.filter(item => item.constraintTypeId === constraintTypeId)}
         renderInput={(params) => <TextField {...params} label="" />}
-        onKeyUp={e => { if (e.keyCode === 13) toggleConstraint(e, e.target.value, constraintTypeId); }}
-        onChange={(event, value) => {
+        onKeyUp={e => {
+          if (e.keyCode === 13) {
+            //On Return 
+            setValue('');
+            toggleConstraint(e, e.target.value, constraintTypeId);
+          }
+        }}
+        onChange={(event, value) => {          
           toggleConstraint(event, value);
-          setValue([]);
         }}
         value={value}
         getOptionLabel={(option) => option.label ?? ''}
-        isOptionEqualToValue={(option, value) => option.label == value.label}
+        isOptionEqualToValue={(option, value) => true}
       />
       <ConstraintList 
         constraints={document.constraints.filter(item => item.constraintTypeId === constraintTypeId) } 
